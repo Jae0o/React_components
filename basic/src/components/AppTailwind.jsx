dbae3e3815ea999fdc0604837d6c154affa06082
Lee Jae Yeong <leey153@naver.com>
2023-05-27 14:50:02 +0900

    Update AppTailwind.jsx

--- basic/src/components/AppTailwind.jsx
+++ basic/src/components/AppTailwind.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AppTailwind() {
     return (
         <div>
-            <h1 className='text-3xl bg-blue-100'> Test Tailwind Css</h1>
+            <h1 className='text-3xl p-5'> Test Tailwind Css</h1>
 
         </div>
     )
